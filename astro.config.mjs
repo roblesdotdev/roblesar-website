a3e6b5f1340fb354a77a96451f160cd20fcf62a9
roblesdotdev <httpsnip@gmail.com>
2024-08-09 16:14:40 -0300

    remove unused boilerplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  })
+  output: "static",
+  // adapter: cloudflare({
+  //   platformProxy: {
+  //     enabled: true
+  //   }
+  // })
 });
