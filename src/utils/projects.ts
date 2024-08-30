7f792086d8c9f12204b7cdc1e51d3fbad602a2d1
roblesdotdev <httpsnip@gmail.com>
2024-08-30 17:20:13 -0300

    add project & links to more

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,7 +1,7 @@
 import { getCollection } from 'astro:content'
 
-export async function getProjects() {
-  return (await getCollection('projects')).sort((a, b) =>
-    a.data.date > b.data.date ? -1 : 1,
-  )
+export async function getProjects(limit?: number) {
+  return (await getCollection('projects'))
+    .sort((a, b) => (a.data.date > b.data.date ? -1 : 1))
+    .slice(0, limit)
 }
